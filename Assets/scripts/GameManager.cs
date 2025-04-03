65da7283c12e778aafcc733ca48b4162f7a8709c
Garth Boyst <garthboyst@gmail.com>
2025-04-03 14:58:03 -0700

    updating game manager to randomly spawn in hunters

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject target;
-    public GameObject[] hunters;
+    public GameObject hunterPrefab;
+    public int numberOfHunters = 3;
+    public float spawnRadius = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
+        Vector2 spawnPos = (Vector2)target.transform.position + Random.insideUnitCircle.normalized * spawnRadius;
+        GameObject hunter = Instantiate(hunterPrefab, spawnPos, Quaternion.identity);
+
+        HunterChaser hc = hunter.GetComponent<HunterChaser>();
+        if (hc != null)
+            hc.target = target.transform;
     }
 
     // Update is called once per frame
