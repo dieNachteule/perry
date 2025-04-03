cb4312f2cdf5bdffe0bf0b61edb0eef257643e48
Garth Boyst <garthboyst@gmail.com>
2025-04-03 15:23:55 -0700

    made header entries for prefabs, arena settings, boundary generation, entity properties

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject target;
+    [Header("Prefabs")]
+    public GameObject targetPrefab;
     public GameObject hunterPrefab;
+    public GameObject wallPrefab;
+
+    [Header("Arena Setttings")]
+    public float arenaWidth = 20f;
+    public float arenaHeight = 20f;
+
+    [Header("Wall Generation")]
+    public int minWallCount = 5;
+    public int maxWallCount = 10;
+    public Vector2 minWallSize = new(1f, 1f);
+    public Vector2 maxWallSize = new(5f, 5f);
+
+    [Header("Entities")]
     public int numberOfHunters = 3;
-    public float spawnRadius = 5f;
+    public float minSpawnSeparation = 5f;
+
+    private Transform target;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
