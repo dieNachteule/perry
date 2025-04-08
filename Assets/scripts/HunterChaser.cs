52288947b6b9a412a90b8e869319747298d9381c
Garth Boyst <garthboyst@gmail.com>
2025-04-07 17:03:12 -0700

    removing unused imports

--- Assets/scripts/HunterChaser.cs
+++ Assets/scripts/HunterChaser.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Concurrent;
-using System.Linq.Expressions;
-using System.Timers;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class HunterChaser : MonoBehaviour
 {
